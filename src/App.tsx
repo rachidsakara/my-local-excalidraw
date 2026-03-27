b5f4f11488ae4334c5c3fefabd52676492268514
Rachid Sakara <rachidsakaraoo7@gmail.com>
2026-03-27 18:23:04 +0100

    Fresh initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Excalidraw } from "@excalidraw/excalidraw";
+import "@excalidraw/excalidraw/index.css";
+
+function App() {
+
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Excalidraw />
+    </div>
+  );
+}
+
+export default App
